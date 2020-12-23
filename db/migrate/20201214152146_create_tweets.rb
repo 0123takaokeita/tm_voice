3df2fa43fe8fe9bb29c74aca4771ee30e4dae679
0123takaokeita <keita.takao@di-v.co.jp>
2020-12-23 22:02:57 +0900

    引き継ぎ

--- db/migrate/20201214152146_create_tweets.rb
+++ db/migrate/20201214152146_create_tweets.rb
@@ -1,6 +1,9 @@
 class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table :tweets do |t|
+      t.string :tweet
+      # t.user   :reference, :foregnkey true
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
