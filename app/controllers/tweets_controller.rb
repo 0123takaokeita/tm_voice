e19e2efcaa3629bfa754db99d44903600442ab6f
takefumi <65272471+0123takaokeita@users.noreply.github.com>
2020-12-24 13:42:33 +0900

    投稿機能完了

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,7 +6,24 @@ class TweetsController < ApplicationController
 
   def new
     @tweet =  Tweet.new
+    # redirect_to root_path
   end
 
+  def create
 
+    tweet = Tweet.new(mitani)
+
+    if tweet.valid?
+      tweet.save
+    else
+      @errors = tweet.errors.full_messages
+      render :new
+    end
+    # redirect_to root_path
+  end
+
+  private
+  def mitani
+    params.require(:tweet).permit(:tweet).merge(user_id: current_user.id)
+  end
 end
